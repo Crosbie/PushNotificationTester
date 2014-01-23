aeeb74b916a64375714cb312f5875ecb639384f8
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2014-01-23 14:02:38 +0530

    add single push support

--- cloud/main.js
+++ cloud/main.js
@@ -3,50 +3,113 @@
  */
 
 
-exports.registerUA = function(params, callback){
+exports.registerUA = function(params, callback) {
   //return callback(null, {test: 'test'});
   var deviceId, platfrom;
-  if(params.deviceToken){
+  if (params.deviceToken) {
     deviceId = params.deviceToken;
     platform = "ios";
-  } else if(params.devicePIN){
+  } else if (params.devicePIN) {
     deviceId = params.devicePIN;
     platform = "blackberry";
-  } else if(params.apid){
+  } else if (params.apid) {
     deviceId = params.apid;
     platform = "android";
   }
   /**
-   * Urbanairship API supports tagging a device. 
+   * Urbanairship API supports tagging a device.
    * To do that, you can pass an extra parameter called "data" with the content specified in http://urbanairship.com/docs/push.html#registration
    */
-  var res = $fh.push({
-    'act':'register',
-    'type':'dev',
-    'params':{
-       'id':deviceId,
-       'platform':platform
+  $fh.push({
+    'act': 'register',
+    'type': 'dev',
+    'params': {
+      'id': deviceId,
+      'platform': platform
     }
   }, function(err, res) {
-  if (err) {
-    console.log(err.toString());
-  } else {
-    console.log("status from UA : " + res.status);
-  }
-});
-  return callback(null, res);
+    if (err) {
+      console.log(err.toString());
+    } else {
+      console.log("status from UA : " + res.status);
+      return callback(null, {
+        'result': 'ok'
+      });
+    }
+  });
 };
 
-exports.pushMessages = function(params, callback){
+// Broadcast message to all registered devices
+exports.pushMessages = function(params, callback) {
   var message = "hello from FH";
   /**
-   * Broadcast a message to all the devices. 
+   * Broadcast a message to all the devices.
    * The format for each platform is different. See http://urbanairship.com/docs/ for API details for each platform.
    */
-  var ios_message = {'aps':{'alert':message}};
-  var android_message = {'android':{'alert':message}};
-  var blackberry_message = {'blackberry':{'content-type':'text/plain', 'body':message}};
-  var res_ios = $fh.push({'act':'broadcast', 'type':'dev', 'params':ios_message});
-  var res_android = $fh.push({'act':'broadcast', 'type':'dev', 'params':android_message});
-  return callback(null,{'result': 'ok'});
+  var ios_message = {
+    'aps': {
+      'alert': message
+    }
+  };
+  var android_message = {
+    'android': {
+      'alert': message
+    }
+  };
+  var blackberry_message = {
+    'blackberry': {
+      'content-type': 'text/plain',
+      'body': message
+    }
+  };
+  var res_ios = $fh.push({
+    'act': 'broadcast',
+    'type': 'dev',
+    'params': ios_message
+  });
+  var res_android = $fh.push({
+    'act': 'broadcast',
+    'type': 'dev',
+    'params': android_message
+  });
+  return callback(null, {
+    'result': 'ok'
+  });
+};
+
+
+// Send direct message to set of registered Devices
+exports.pushDirectMessage = function(params, callback) {
+  console.log("PARAMS",params);
+  var message = "direct message";
+  var deviceToken = params.deviceID;
+
+  // var ios_message = {
+  //   'device_tokens': [deviceToken],
+  //   'aps': {
+  //     'alert': message
+  //   }
+  // };
+
+  var android_message = {
+    'device_tokens': [deviceToken],
+    'android': {
+      'alert': message
+    }
+  };
+
+  $fh.push({
+    'act': 'push',
+    'type': 'dev',
+    'params': android_message
+  }, function(err, res) {
+    if (err) {
+      console.log(err.toString());
+    } else {
+      console.log("status from UA : " + res.status);
+      return callback(null, {
+        'result': 'ok'
+      });
+    }
+  });
 };
